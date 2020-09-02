d179d68ee314596ec9e907d44b2c805a41a18e12
Gian van der Spuy <gvds@sun.ac.za>
2020-09-02 11:45:26 +0200

    Removes unwanted js components and installs local Nanito fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+// const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
-    // content: [
-    //   './app/**/*.php',
-    //   './resources/**/*.html',
-    //   './resources/**/*.js',
-    //   './resources/**/*.jsx',
-    //   './resources/**/*.ts',
-    //   './resources/**/*.tsx',
-    //   './resources/**/*.php',
-    //   './resources/**/*.vue',
-    //   './resources/**/*.twig',
-    // ],
+    content: [
+      './app/**/*.php',
+      './resources/**/*.html',
+      './resources/**/*.js',
+      './resources/**/*.jsx',
+      './resources/**/*.ts',
+      './resources/**/*.tsx',
+      './resources/**/*.php',
+    ],
     options: {
       // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
       // whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
@@ -23,9 +21,23 @@ module.exports = {
   },
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      colors: {
+        gray: {
+          '50' : '#f9fafb',
+          '100': '#f4f5f7',
+          '200': '#e5e7eb',
+          '300': '#d2d6dc',
+          '400': '#9fa6b2',
+          '500': '#6b7280',
+          '600': '#4b5563',
+          '700': '#374151',
+          '800': '#252f3f',
+          '900': '#161e2e',
+        },
       },
+      // fontFamily: {
+      //   sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      // },
     },
   },
   variants: {
@@ -33,6 +45,6 @@ module.exports = {
   },
   plugins: [
     // require('@tailwindcss/custom-forms'),
-    require('@tailwindcss/ui'),
+    // require('@tailwindcss/ui'),
   ],
 };
