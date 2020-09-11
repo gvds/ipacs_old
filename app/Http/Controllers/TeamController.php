525547bdcebe78f4ea5a113807256a6335f33735
Gian van der Spuy <gvds@sun.ac.za>
2020-09-11 09:31:50 +0200

    Implements token management for new redcap project users

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Team;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -101,9 +102,60 @@ class TeamController extends Controller
             'user' => 'required|integer|exists:App\User,id',
             'site' => 'nullable'
         ]);
-        $team = Team::findOrFail(session('currentProject'));
-        $team->users()->sync([$request->user => ['site_id' => $request->site]], false);
+        try {
+            DB::beginTransaction();
+            $project = $request->currentProject;
+            $user = User::findOrFail($request->user);
+            if (!empty($project->redcapProject_id)) {
+                $redcap_user = $this->getREDCapUser($project->redcapProject_id, $user->username);
+                if (empty($redcap_user)) {
+                    throw new Exception('This user was not found in the REDCap project');
+                }
+                $token = $this->getOrGenerateToken($redcap_user, $project, $user);
+            }
+            $team = Team::findOrFail($project->id);
+            $team->users()->sync([$user->id => ['site_id' => $request->site]], false);
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return back()->withInput()->withErrors($th->getMessage());
+        }
         return redirect('/team');
+    }
+
+    private function getREDCapUser($redcapProject_id, $username)
+    {
+        return DB::connection('redcap')->select(
+            "SELECT * from redcap_user_rights left join redcap_data_access_groups on 
+                    redcap_user_rights.group_id = redcap_data_access_groups.group_id and 
+                    redcap_user_rights.project_id = redcap_data_access_groups.project_id 
+                    where redcap_user_rights.project_id = $redcapProject_id and 
+                    redcap_user_rights.username = '$username'"
+        );
+    }
+
+    private function getOrGenerateToken($redcap_user, $project, $user)
+    {
+        if (is_null($redcap_user[0]->api_token)) {
+            for ($i = 0; $i < 5; $i++) {
+                $token = strtoupper(bin2hex(random_bytes(16)));
+                $duplicate = DB::connection('redcap')->select(
+                    "SELECT count(api_token) as found FROM redcap_user_rights WHERE api_token = '$token'"
+                );
+                if (!$duplicate[0]->found) {
+                    break;
+                }
+                throw new Exception('Could not create unique API token for this user in the REDCap database');
+            }
+            DB::connection('redcap')->update(
+                "UPDATE redcap_user_rights SET api_token = '$token' WHERE 
+                redcap_user_rights.project_id = $project->redcapProject_id and 
+                redcap_user_rights.username = '$user->username'"
+            );
+            return $token;
+        } else {
+            return $redcap_user[0]->api_token;
+        }
     }
 
     /**
@@ -150,7 +202,7 @@ class TeamController extends Controller
         $team->users()->sync([$user->id => [
             'site_id' => $request->site,
             'redcap_api_token' => $request->redcap_api_token
-            ]], false);
+        ]], false);
         return redirect('/team');
     }
 
@@ -187,7 +239,7 @@ class TeamController extends Controller
             ],
         ])->validate();
         $team = Team::findOrFail(session('currentProject'));
-        $user->syncPermissions($validatedData,$team->id);
+        $user->syncPermissions($validatedData, $team->id);
         $user->flushCache();
         return redirect("/team/$user->id");
     }
