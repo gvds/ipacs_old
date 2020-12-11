e5944049fa1fecdbf1d339f4ed25dabeaf45e3b9
Gian van der Spuy <gvds@sun.ac.za>
2020-12-11 16:50:44 +0200

    Adds ability to check multiple permissions to middleware

--- app/Http/Middleware/CheckProjectPermissions.php
+++ app/Http/Middleware/CheckProjectPermissions.php
@@ -16,14 +16,15 @@ class CheckProjectPermissions
      * @return mixed
      */
 
-    public function handle($request, Closure $next, $permission)
+    public function handle($request, Closure $next, $permissions)
     {
         $this->currentProject = \App\project::find(session('currentProject', null));
         if (is_null($this->currentProject)) {
             return redirect('/')->with('warning', 'There is currently no selected project');
         }
         $user = auth()->user();
-        if ($user->hasRole('sysadmin') or $user->isAbleTo($permission, $this->currentProject->team->name) or $user->owns($this->currentProject, 'owner')) {
+        // if ($user->hasRole('sysadmin') or $user->isAbleTo($permission, $this->currentProject->team->name) or $user->owns($this->currentProject, 'owner')) {
+        if ($user->isAbleTo([$permissions], $this->currentProject->team->name)) {
             $request->request->add(['currentProject' => $this->currentProject]);
         } else {
             return redirect('/')->with('error', 'You do not have the necessary rights to do access that function in this project');
