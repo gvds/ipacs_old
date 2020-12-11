38c122549d811ddcdf5fd6051506d293460c39e0
Gian van der Spuy <gvds@sun.ac.za>
2020-12-11 13:04:09 +0200

    New progress controller

---
+++ app/Http/Controllers/ProgressController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\subject;
+use Illuminate\Http\Request;
+
+class ProgressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subjects = subject::where('project_id',session('currentProject'))
+        ->with('events')
+        ->get();
+        $eventStatuses = \App\eventStatus::pluck('eventstatus','id');
+        return view('/progress.index', compact('subjects','eventStatuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(subject $subject)
+    {
+        //
+    }
+}
---
+++ resources/views/progress/index.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+
+    <x-pageheader>
+        Progress Report
+        {{-- <x-slot name='button'>
+            <x-buttonlink href="project/create">
+                New Project
+            </x-buttonlink>
+        </x-slot> --}}
+        {{-- <x-slot name='button2'>
+            <x-buttonlink href="redcapproject/new">
+                New REDCap Project
+            </x-buttonlink>
+        </x-slot> --}}
+    </x-pageheader>
+
+    <x-table>
+        <x-slot name='head'>
+            <th>ID</th>
+            <th>Site</th>
+            <th>Current Arm</th>
+        </x-slot>
+        @foreach ($subjects as $subject)
+            <tr class='odd:bg-gray-200'>
+                <td>{{$subject->subjectID}}</td>
+                <td>{{$subject->site->name}}</td>
+                <td>{{$subject->arm->name}}</td>
+                {{-- <table> --}}
+                    {{-- <tr> --}}
+                        @foreach ($subject->events as $event)
+                        <td>{{$event->arm->name}}<br/>{{$event->name}}<br />{{$eventStatuses[$event->pivot->eventstatus_id]}}</td>
+                            
+                        @endforeach
+                    {{-- </tr> --}}
+                {{-- </table> --}}
+            </tr>
+        @endforeach
+    </x-table>
+
+</x-layout>
