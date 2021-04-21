62092c85a9384eb2d2a0385c71cd51bf32d8598c
Gian van der Spuy <gvds@sun.ac.za>
2021-04-21 16:24:32 +0200

    New Unit Definition show page layout

--- resources/views/storage/unitdef/show.blade.php
+++ resources/views/storage/unitdef/show.blade.php
@@ -8,82 +8,98 @@
 
     @include('layouts.message')
 
-    <div class='flex flex-row' x-data="deleteModal()">
-        <div>
-            <x-table>
-                <tr>
-                    <th class='text-left'>Unit Type</th>
-                    <td>{{$unitDefinition->unitType}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Orientation</th>
-                    <td>{{$unitDefinition->orientation}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Sections</th>
-                    <td>{{count($sections)}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Racks</th>
-                    <td>{{$unitDefinition->racks}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Boxes</th>
-                    <td>{{$unitDefinition->boxes}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Section Layout</th>
-                    <td>{{$unitDefinition->sectionLayout}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Box Designation</th>
-                    <td>{{$unitDefinition->boxDesignation}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Storage Type</th>
-                    <td>{{$unitDefinition->storageType}}</td>
-                </tr>
-                <tr>
-                    <th class='text-left'>Rack Order</th>
-                    <td>{{$unitDefinition->rackOrder}}</td>
-                </tr>
-            </x-table>
+    <div class='flex flex-col' x-data="deleteModal()">
+        <div class='flex flex-row mb-5'>
             <div>
-                <button class='bg-red-700 text-red-100 py-1 px-2 rounded-md font-bold'
-                    @click="deleteconf('unitDefinition','{{$unitDefinition->unitType}}',{{$unitDefinition->id}})">Delete</button>
+                <x-table>
+                    <tr>
+                        <th class='text-left'>Unit Type</th>
+                        <td>{{$unitDefinition->unitType}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Orientation</th>
+                        <td>{{$unitDefinition->orientation}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Sections</th>
+                        <td>{{count($sections)}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Racks</th>
+                        <td>{{$unitDefinition->racks}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Boxes</th>
+                        <td>{{$unitDefinition->boxes}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Section Layout</th>
+                        <td>{{$unitDefinition->sectionLayout}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Box Designation</th>
+                        <td>{{$unitDefinition->boxDesignation}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Storage Type</th>
+                        <td>{{$unitDefinition->storageType}}</td>
+                    </tr>
+                    <tr>
+                        <th class='text-left'>Rack Order</th>
+                        <td>{{$unitDefinition->rackOrder}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan=2>
+                            <button class='bg-red-700 text-red-100 py-1 px-2 rounded-md font-bold w-full'
+                                @click="deleteconf('unitDefinition','{{$unitDefinition->unitType}}',{{$unitDefinition->id}})">Delete</button>
+                        </td>
+                    </tr>
+                </x-table>
+            </div>
+            <div class='ml-20'>
+                <div class="flex justify-between">
+                    <div class='font-medium text-lg'>Sections</div>
+                    <div>
+                        {{ Form::open(['url' => '/section/create', 'class' => '', 'method' => 'GET']) }}
+                        {{ Form::hidden('unitDefinition_id', $unitDefinition->id) }}
+                        {{ Form::submit('Add New Section', ['class'=>'text-sm leading-none']) }}
+                        {{ Form::close() }}
+                    </div>
+                </div>
+                <x-table>
+                    <x-slot name='head'>
+                        <th>Section</th>
+                        <th>Rows</th>
+                        <th>Columns</th>
+                        <th>Boxes</th>
+                        <th>Positions</th>
+                    </x-slot>
+                    @foreach ($sections as $section)
+                    <tr class="odd:bg-gray-100">
+                        <td>{{$section->section}}</td>
+                        <td>{{$section->rows}}</td>
+                        <td>{{$section->columns}}</td>
+                        <td>{{$section->boxes}}</td>
+                        <td>{{$section->positions}}</td>
+                        <td>
+                            <button class='bg-red-700 text-red-100 py-1 px-2 rounded-md font-bold'
+                                @click="deleteconf('section','{{$section->section}}',{{$section->id}})">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </x-table>
             </div>
         </div>
-        <div class='flex flex-col ml-20'>
-            <div class='font-medium text-lg'>Sections</div>
-            <x-table>
-                <x-slot name='head'>
-                    <th>Section</th>
-                    <th>Rows</th>
-                    <th>Columns</th>
-                    <th>Boxes</th>
-                    <th>Positions</th>
-                </x-slot>
-                @foreach ($sections as $section)
-                <tr class="odd:bg-gray-100">
-                    <td>{{$section->section}}</td>
-                    <td>{{$section->rows}}</td>
-                    <td>{{$section->columns}}</td>
-                    <td>{{$section->boxes}}</td>
-                    <td>{{$section->positions}}</td>
-                    <td>
-                        <button class='bg-red-700 text-red-100 py-1 px-2 rounded-md font-bold'
-                            @click="deleteconf('section','{{$section->section}}',{{$section->id}})">Delete</button>
-                    </td>
-                </tr>
-                @endforeach
-            </x-table>
-            {{ Form::open(['url' => '/section/create', 'class' => '', 'method' => 'GET']) }}
-            {{ Form::hidden('unitDefinition_id', $unitDefinition->id) }}
-            {{ Form::submit('Add New Section', ['class'=>'text-sm']) }}
-            {{ Form::close() }}
-        </div>
-        <div class='flex flex-col ml-20'>
-            <div class='font-medium text-lg'>Physical Storage Units</div>
+        <div class='flex flex-col'>
+            <div class='flex justify-between'>
+                <div class='font-medium text-lg'>Physical Storage Units</div>
+                <div>
+                    {{ Form::open(['url' => '/physicalUnit/create', 'class' => '', 'method' => 'GET']) }}
+                    {{ Form::hidden('unitDefinition_id', $unitDefinition->id) }}
+                    {{ Form::submit('Add New Unit', ['class'=>'text-sm leading-none']) }}
+                    {{ Form::close() }}
+                </div>
+            </div>
             <x-table>
                 <x-slot name='head'>
                     <th>Unit ID</th>
@@ -118,10 +134,6 @@
                 </tr>
                 @endforeach
             </x-table>
-            {{ Form::open(['url' => '/physicalUnit/create', 'class' => '', 'method' => 'GET']) }}
-            {{ Form::hidden('unitDefinition_id', $unitDefinition->id) }}
-            {{ Form::submit('Add New Unit', ['class'=>'text-sm']) }}
-            {{ Form::close() }}
         </div>
         <x-modals.deleteModal />
     </div>
