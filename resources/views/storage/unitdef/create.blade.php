d2e43c048d000c036872dc41d03ebee657bcb255
Gian van der Spuy <gvds@sun.ac.za>
2021-07-14 15:47:28 +0200

    Fixes boxDesignation dropdown list error in unitdefinition create

--- resources/views/storage/unitdef/create.blade.php
+++ resources/views/storage/unitdef/create.blade.php
@@ -13,16 +13,10 @@
     {{ Form::text('unitType', null, ['required']) }}
     {{ Form::label('orientation', 'Orientation', ['class'=>'text-sm']) }}
     {{ Form::select('orientation', ['Chest'=>'Chest','Upright'=>'Upright'], ['required']) }}
-    {{-- {{ Form::label('sections', 'Sections', ['class'=>'text-sm']) }}
-    {{ Form::text('sections', null, ['required']) }} --}}
-    {{-- {{ Form::label('racks', 'Racks', ['class'=>'text-sm']) }}
-    {{ Form::number('racks', null, ['required']) }}
-    {{ Form::label('boxes', 'Boxes', ['class'=>'text-sm']) }}
-    {{ Form::text('boxes', null, ['required']) }} --}}
     {{ Form::label('sectionLayout', 'Section Layout', ['class'=>'text-sm']) }}
     {{ Form::select('sectionLayout', ['Vertical'=>'Vertical','Horizontal'=>'Horizontal'], ['required']) }}
     {{ Form::label('boxDesignation', 'Box Designation', ['class'=>'text-sm']) }}
-    {{ Form::select('boxDesignation', ['Alpha'=>'Alpha','Numeric','Numeric'], ['required']) }}
+    {{ Form::select('boxDesignation', ['Alpha'=>'Alpha','Numeric'=>'Numeric'], ['required']) }}
     {{ Form::label('storageType', 'Storage Type', ['class'=>'text-sm']) }}
     {{ Form::select('storageType', ['Minus 80'=>'Minus 80','Liquid Nitrogen'=>'Liquid Nitrogen','Minus 20'=>'Minus 20','BiOS'=>'BiOS'], ['required']) }}
     {{ Form::label('rackOrder', 'Rack Order', ['class'=>'text-sm']) }}
