82e2bf5b21d0e19da949ce75526b67edca576c1d
Gian van der Spuy <gvds@sun.ac.za>
2023-01-19 12:15:24 +0200

    Remove Factory from samplelog model

--- app/samplelog.php
+++ app/samplelog.php
@@ -2,12 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class samplelog extends Model
 {
-    use HasFactory;
 
     protected $guarded = [];
 }
